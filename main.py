7134a0ac7dc78c273b1faf3d21e882cea3ceb550
LeoAnderssonKTH <132525577+LeoAnderssonKTH@users.noreply.github.com>
2024-12-19 17:32:37 +0100

    looking for bug fix, can´t see tile to the right

--- main.py
+++ main.py
@@ -1,32 +1,46 @@
 import pygame
-from othello.globals import SCREEN_WIDTH, SCREEN_HEIGHT
+from othello.globals import SCREEN_WIDTH, SCREEN_HEIGHT, SQUARE_SIZE
 from othello.board import Board
 SCREEN = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 pygame.display.set_caption('Othello')
 
-FPS = 60
+FPS = 5
+
+def get_mouse_pos(pos):
+    x, y = pos
+    row = y // SQUARE_SIZE
+    col = x // SQUARE_SIZE
+    return row, col
 
 def main():
     run = True
     clock = pygame.time.Clock()
     board = Board()
-    
 
     while run:
         clock.tick(FPS)
-
+        
+        # Handle events first
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
 
             if event.type == pygame.MOUSEBUTTONDOWN:
-                pass
-                
+                pos = pygame.mouse.get_pos()
+                row, col = get_mouse_pos(pos)
+                #print(valid_moves)
+                #board.print_board()
+
+                # Check if the clicked position is a valid move
+                if (row, col) in valid_moves:
+                    board.make_move(row, col)
+
+        # Update the board display
         board.draw_tiles(SCREEN)
-        board.valid_moves(SCREEN)
+        valid_moves = board.valid_moves(SCREEN)  # Re-calculate valid moves
         
         pygame.display.update()
 
-    pygame.quit()  # This should be aligned with the `while run:` block
+    pygame.quit()
 
 main()
--- othello/board.py
+++ othello/board.py
@@ -36,18 +36,6 @@ class Board:
                     self.board[row].append(Tile(row, col, BLACK))
                 elif row == 4 and col == 4:
                     self.board[row].append(Tile(row, col, WHITE))
-                elif row == 2 and col == 3:
-                    self.board[row].append(Tile(row, col, WHITE))
-                elif row == 2 and col == 5:
-                    self.board[row].append(Tile(row, col, WHITE))
-                elif row == 5 and col == 2:
-                    self.board[row].append(Tile(row, col, WHITE))
-                elif row == 4 and col == 5:
-                    self.board[row].append(Tile(row, col, WHITE))
-                elif row == 4 and col == 6:
-                    self.board[row].append(Tile(row, col, WHITE))
-                elif row == 4 and col == 7:
-                    self.board[row].append(Tile(row, col, WHITE))
                 else:
                     self.board[row].append(0)
             else:
@@ -70,134 +58,207 @@ class Board:
         return list_of_tiles
 
     def check_up(self, tile, opposite_color):
-        #print("Checking Tile At: ", tile.row, tile.column)
+        
         up = tile.row - 1
-        if self.board[up][tile.column] != 0:
-            while self.board[up][tile.column].color == opposite_color and up > 0:
+
+        if up < 0:
+            return None
+
+        if self.board[up][tile.column] == 0:
+            return None
+
+        if self.board[up][tile.column].color != opposite_color:
+            return None
+        
+        while self.board[up][tile.column].color == opposite_color and up > 0:
                 
-                up -= 1
-                if self.board[up][tile.column] == 0:
-                    break
-            
+            up -= 1
             if self.board[up][tile.column] == 0:
                 return (up, tile.column)
+            
+        return None
+                
 
     def check_down(self, tile, opposite_color):
-        #print("Checking Tile At: ", tile.row, tile.column)
+
         down = tile.row + 1
-        if self.board[down][tile.column] != 0:
-            while self.board[down][tile.column].color == opposite_color and down < ROWS-1:
+
+        if down > ROWS - 1:
+            return None
+        
+        if self.board[down][tile.column] == 0:
+            return None
+        
+        
+        if self.board[down][tile.column].color != opposite_color:
+            return None
+        
+       
+        while self.board[down][tile.column].color == opposite_color and down < ROWS-1:
                 
-                down += 1
-                if self.board[down][tile.column] == 0:
-                    break
-            
+            down += 1
             if self.board[down][tile.column] == 0:
                 return (down, tile.column)
+            
+        return None
 
     def check_left(self, tile, opposite_color):
-        #print("Checking Tile At: ", tile.row, tile.column)
         left = tile.column - 1
-        if self.board[tile.row][left] != 0:
-            while self.board[tile.row][left].color == opposite_color and left > 0:
+
+        if left < 0:
+            return None
+
+        if self.board[tile.row][left] == 0:
+            return None
+        if self.board[tile.row][left].color != opposite_color:
+            return None
+        
+        
+        while self.board[tile.row][left].color == opposite_color and left > 0:
                 
-                left -= 1
-                if self.board[tile.row][left] == 0:
-                    break
-            
+            left -= 1
             if self.board[tile.row][left] == 0:
                 return (tile.row, left)
+            
+        return None
+                
 
     def check_right(self, tile, opposite_color):
-        #print("Checking Tile At: ", tile.row, tile.column)
         right = tile.column + 1
-        if self.board[tile.row][right] != 0:
-            while self.board[tile.row][right].color == opposite_color and right < COLUMNS-1:
+        print(tile.column)
+
+        if right > COLUMNS - 1:
+            return None
+        
+        if self.board[tile.row][right] == 0:
+            return None
+        
+        if self.board[tile.row][right].color != opposite_color:
+            return None
+        
+        while self.board[tile.row][right].color == opposite_color and right < COLUMNS-1:
                 
-                right += 1
-                if self.board[tile.row][right] == 0:
-                    break
-            
+            right += 1
             if self.board[tile.row][right] == 0:
                 return (tile.row, right)
 
+        return None  
+                
+
     def check_left_up(self, tile, opposite_color):
-        #print("Checking Tile At: ", tile.row, tile.column)
         left = tile.column - 1
         up = tile.row - 1
-        if self.board[up][left] != 0:
-            while self.board[up][left].color == opposite_color and left > 0 and up > 0:
+
+        if up < 0 or left < 0:
+            return None
+
+        if self.board[up][left] == 0:
+            return None
+        
+        if self.board[up][left].color != opposite_color:
+            return None
+        
+        while self.board[up][left].color == opposite_color and left > 0 and up > 0:
                 
-                left -= 1
-                up -= 1
-                if self.board[up][left] == 0:
-                    break
-            
+            left -= 1
+            up -= 1
             if self.board[up][left] == 0:
                 return (up, left)
+        
+        return None
+                
 
     def check_right_up(self, tile, opposite_color):
-        #print("Checking Tile At: ", tile.row, tile.column)
         right = tile.column + 1
         up = tile.row - 1
-        if self.board[up][right] != 0:
-            while self.board[up][right].color == opposite_color and right < COLUMNS-1 and up > 0:
+
+        if right > COLUMNS - 1 or up < 0:
+            return None
+
+        if self.board[up][right] == 0:
+            return None
+        if self.board[up][right].color != opposite_color:
+            return None
+       
+        while self.board[up][right].color == opposite_color and right < COLUMNS-1 and up > 0:
                 
-                right += 1
-                up -= 1
-                if self.board[up][right] == 0:
-                    break
-            
+            right += 1
+            up -= 1
             if self.board[up][right] == 0:
                 return (up, right)
+            
+        return None
+                
 
     def check_left_down(self, tile, opposite_color):
-        #print("Checking Tile At: ", tile.row, tile.column)
         left = tile.column - 1
         down = tile.row + 1
-        if self.board[down][left] != 0:
-            while self.board[down][left].color == opposite_color and left > 0 and down < ROWS-1:
+
+        if left < 0 or down > ROWS - 1:
+            return None
+
+        if self.board[down][left] == 0:
+            return None
+        if self.board[down][left].color != opposite_color:
+            return None
+
+        
+        while self.board[down][left].color == opposite_color and left > 0 and down < ROWS-1:
                 
-                left -= 1
-                down += 1
-                if self.board[down][left] == 0:
-                    break
-            
+            left -= 1
+            down += 1
             if self.board[down][left] == 0:
                 return (down, left)
+            
+        return None
+                
 
     def check_right_down(self, tile, opposite_color):
-        #print("Checking Tile At: ", tile.row, tile.column)
         right = tile.column + 1
         down = tile.row + 1
-        if self.board[down][right] != 0:
-            while self.board[down][right].color == opposite_color and right < COLUMNS-1 and down < ROWS-1:
+
+        if right > COLUMNS - 1 or down > ROWS - 1:
+            return None
+
+        if self.board[down][right] == 0:
+            return None
+        if self.board[down][right].color != opposite_color:
+            return None
+        
+        while self.board[down][right].color == opposite_color and right < COLUMNS-1 and down < ROWS-1:
                 
-                right += 1
-                down += 1
-                if self.board[down][right] == 0:
-                    break
-            
+            right += 1
+            down += 1
             if self.board[down][right] == 0:
                 return (down, right)
+        
+        return None
+                
     
     def find_moves(self, tile, opposite_color):
         possible_moves = []
-        if up := self.check_up(tile, opposite_color):
+        if (up := self.check_up(tile, opposite_color)) and tile.row != 0:
             possible_moves.append(up)
-        if down := self.check_down(tile, opposite_color):
+
+        if (down := self.check_down(tile, opposite_color)) and tile.row != ROWS - 1:
             possible_moves.append(down)
-        if left := self.check_left(tile, opposite_color):
+
+        if (left := self.check_left(tile, opposite_color)) and tile.column != 0:
             possible_moves.append(left)
-        if right := self.check_right(tile, opposite_color):
+
+        if (right := self.check_right(tile, opposite_color)) and tile.column != COLUMNS - 1:
             possible_moves.append(right)
-        if left_up := self.check_left_up(tile, opposite_color):
+
+        if (left_up := self.check_left_up(tile, opposite_color)) and tile.row != 0 and tile.column != 0:
             possible_moves.append(left_up)
-        if right_up := self.check_right_up(tile, opposite_color):
+
+        if (right_up := self.check_right_up(tile, opposite_color)) and tile.row != 0 and tile.column != COLUMNS - 1:
             possible_moves.append(right_up)
-        if left_down := self.check_left_down(tile, opposite_color):
+
+        if (left_down := self.check_left_down(tile, opposite_color)) and tile.row != ROWS - 1 and tile.column !=0:
             possible_moves.append(left_down)
-        if right_down := self.check_right_down(tile, opposite_color):
+
+        if (right_down := self.check_right_down(tile, opposite_color)) and tile.row != ROWS - 1 and tile.column != COLUMNS - 1:
             possible_moves.append(right_down)
 
         if possible_moves:
@@ -223,6 +284,8 @@ class Board:
             valid_moves = [item for sublist in valid_moves for item in sublist] #collapses the earlier valid_moves
             #print(valid_moves)
             self.print_moves(valid_moves, screen)
+        
+        return valid_moves
 
     def print_moves(self, valid_moves, screen):
 
@@ -236,7 +299,112 @@ class Board:
             pygame.draw.circle(screen, YELLOW, (x, y), radius)
             
             
+    def make_move(self, row, col):
+        if self.blacks_turn == True:
+            self.board[row].insert(col, Tile(row, col, BLACK))
+            self.flip_tiles(row, col)
+            self.blacks_turn = False 
+        else:
+            self.board[row].insert(col, Tile(row, col, WHITE))
+            self.flip_tiles(row, col)
+            self.blacks_turn = True
+
+    def flip_tiles(self, row, col):
+        
+        if row != 0 and hasattr(self.board[row - 1][col], 'color'):
+            self.flip_up(row, col)
+
+        if row != ROWS - 1 and hasattr(self.board[row + 1][col], 'color'):
+            self.flip_down(row, col)
+
+        if col != 0 and hasattr(self.board[row][col - 1], 'color'):
+            self.flip_left(row, col)
+
+        print("up", hasattr(self.board[row -1][col], 'color'))
+        print("down", hasattr(self.board[row +1][col], 'color'))
+        print("left", hasattr(self.board[row][col - 1], 'color'))
+        print("right", hasattr(self.board[row][col + 1], 'color'))
+        print()
+        if col != COLUMNS - 1 and hasattr(self.board[row][col + 1], 'color'):
+            self.flip_right(row, col)
+
+    def flip_up(self, row, col):
+        tiles_to_flip = []
+        up = row - 1
+        
+        while self.board[up][col].color != self.board[row][col].color:
+            tiles_to_flip.append(self.board[up][col])
+            #print(tiles_to_flip[0].color)
+            up -= 1
+            if(self.board[up][col] == 0):
+                break
             
+            if self.board[up][col].color == self.board[row][col].color:
+                for tile in tiles_to_flip:
+                    tile.color = self.board[row][col].color
+
+    def flip_down(self, row, col):
+        tiles_to_flip = []
+        down = row + 1
+        
+        while self.board[down][col].color != self.board[row][col].color:
+            tiles_to_flip.append(self.board[down][col])
+            #print(tiles_to_flip[0].color)
+            down += 1
+            if(self.board[down][col] == 0):
+                break
+            
+            if self.board[down][col].color == self.board[row][col].color:
+                for tile in tiles_to_flip:
+                    tile.color = self.board[row][col].color
+
+    def flip_left(self, row, col):
+        tiles_to_flip = []
+        left = col - 1
+        
+        while self.board[row][left].color != self.board[row][col].color:
+            tiles_to_flip.append(self.board[row][left])
+            #print(tiles_to_flip[0].color)
+            left -= 1
+            if(self.board[row][left] == 0):
+                break
+            
+            if self.board[row][left].color == self.board[row][col].color:
+                for tile in tiles_to_flip:
+                    tile.color = self.board[row][col].color
+
+    def flip_right(self, row, col):
+        tiles_to_flip = []
+        right = col + 1
+        
+        while self.board[row][right].color != self.board[row][col].color:
+            tiles_to_flip.append(self.board[row][right])
+            #print(tiles_to_flip[0].color)
+            right += 1
+            if(self.board[row][right] == 0):
+                break
+            
+            if self.board[row][right].color == self.board[row][col].color:
+                for tile in tiles_to_flip:
+                    tile.color = self.board[row][col].color
+
+    def print_board(self):
+    
+        for row in self.board:
+            row_str = ""
+            for tile in row:
+                if tile == 0:  # Empty space
+                    row_str += "0 "  # Print '0' for empty space
+                else:
+                    row_str += f"{tile.color} "  # Print the color of the tile
+            print(row_str)  # Strip any trailing space for clean output
+                
+        print()
+
+
+        
+            
+
             
                         
 
